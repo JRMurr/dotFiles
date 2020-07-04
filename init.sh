2b7e8ab61e6cde6b11aee14a329bc120deef44a5
John Murray <5672686+JRMurr@users.noreply.github.com>
2020-07-04 12:22:55 -0400

    setup init script and gitconfig

--- init.sh
+++ init.sh
@@ -12,3 +12,12 @@ delete_and_link() {
     rm -rf $link_path
     ln -s $script_dir/$dest_path $link_path
 }
+
+# Fish
+echo "Initializing fish..."
+delete_and_link ~/.config/fish fish
+echo "  You'll need to install fish and set it as your shell manually"
+
+# Git
+echo "Initializing git..."
+delete_and_link ~/.gitconfig gitconfig
