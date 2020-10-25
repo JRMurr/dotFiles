8fe8791c3966f867c67087250c3fd6d973c3362b
John Murray <5672686+JRMurr@users.noreply.github.com>
2020-10-25 11:38:18 -0400

    add resetPulse...

--- fish/config.nixos-john.fish
+++ fish/config.nixos-john.fish
@@ -6,3 +6,13 @@ set -x BROWSER "firefox"
 
 
 alias nixRe="sudo nixos-rebuild switch"
+
+
+function resetPulse
+    # https://wiki.archlinux.org/index.php/PulseAudio/Troubleshooting#Bad_configuration_files
+    killall pulseaudio
+    set files /tmp/pulse* ~/.pulse* ~/.config/pulse
+    rm -rf $files
+    pulseaudio -k
+    pulseaudio --start
+end
